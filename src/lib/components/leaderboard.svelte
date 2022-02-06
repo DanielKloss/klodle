9d4d157bdf6ecbad1375f0d2ba9b77667ae78045
DanielKloss <arsenal_fan_dan@hotmail.com>
2022-02-06 20:04:29 +0000

    changed sorting order

--- src/lib/components/leaderboard.svelte
+++ src/lib/components/leaderboard.svelte
@@ -9,52 +9,39 @@
     $: $todaySelected, leaderboardPlayers = leaderboardPlayers.sort(sortPlayers);
 
     function sortPlayers(a, b) { 
-		let firstSort;
-        let secondSort;
-
 		if ($todaySelected) {
 			if(!isFinite(a.todaysScore) && isFinite(b.todaysScore)) {
         		return 1;
-    		}
-		
-    		if(!isFinite(b.todaysScore) && isFinite(a.todaysScore)) {
+    		} else if(!isFinite(b.todaysScore) && isFinite(a.todaysScore)) {
         		return -1;
-    		}
-
-			firstSort = a.todaysScore - b.todaysScore;
-
-            if (firstSort != 0){
-                return firstSort;
+    		}else if (a.todaysScore - b.todaysScore != 0){
+                return a.todaysScore - b.todaysScore;
+            } else if (a.dailyGold - b.dailyGold != 0){
+                return a.dailyGold - b.dailyGold;
+            } else if (a.dailySilver - b.dailySilver != 0){
+                return a.dailySilver - b.dailySilver;
+            } else if (a.dailyBronze - b.dailyBronze){
+                return a.dailyBronze - b.dailyBronze;
+            } else {
+                return a.todaysTime - b.todaysTime;
             }
-
-            if (a.todaysTime > b.todaysTime){
-			    return 1;
-		    } else if (b.todaysTime > a.todaysTime) {
-			    return -1;
-		    }
 		} else {
-			if(!isFinite(a.averageScore.toFixed(1)) && isFinite(b.averageScore.toFixed(1))) {
+			if(!isFinite(a.averageScore.toFixed(2)) && isFinite(b.averageScore.toFixed(2))) {
         		return 1;
-    		}
-		
-    		if(!isFinite(b.averageScore.toFixed(1)) && isFinite(a.averageScore.toFixed(1))) {
+    		} else if(!isFinite(b.averageScore.toFixed(2)) && isFinite(a.averageScore.toFixed(2))) {
         		return -1;
-    		}
-			
-			firstSort = a.averageScore.toFixed(1) - b.averageScore.toFixed(1);
-
-            if (firstSort != 0){
-                return firstSort;
-            }
-
-            secondSort = b.numberOfGames - a.numberOfGames;
-
-            if (secondSort != 0){
-                return secondSort;
+    		} else if (a.averageScore.toFixed(2) - b.averageScore.toFixed(2) != 0){
+                return a.averageScore.toFixed(2) - b.averageScore.toFixed(2);
+            } else if (b.numberOfGames - a.numberOfGames != 0){
+                return b.numberOfGames - a.numberOfGames;
+            } else if (b.dailyGold - a.dailyGold != 0){
+                return b.dailyGold - a.dailyGold;
+            } else if (b.dailySilver - a.dailySilver != 0){
+                return b.dailySilver - a.dailySilver;
+            } else {
+                return b.dailyBronze - a.dailyBronze
             }
 		}
-
-		return 0;
 	};
 
     leaderboardPlayers = leaderboardPlayers.sort(sortPlayers);
@@ -98,7 +85,7 @@
             <p class="playerScore">-</p>
             {:else}
             <div>
-                <p class="playerScore">{player.averageScore.toFixed(1)}</p>
+                <p class="playerScore">{player.averageScore.toFixed(2)}</p>
                 {#if player.numberOfGames > 1}
                 <p class="scoreDetail">from {player.numberOfGames} games</p>
                 {:else}
@@ -115,9 +102,9 @@
     </a>
     {/each}
     {#if $todaySelected}
-    <p class="sortedExplanation">Sorted by: <b>Score</b> then by <b>Time Completed</b></p>
+    <p class="sortedExplanation">Sorted by: <b>1) Score</b> then by <b>2) Fewest Gold Medals 3) Fewest Silver Medals 4) Fewest Bronze Medals</b> then by <b>5) Time Completed</b></p>
     {:else}
-    <p class="sortedExplanation">Sorted by: <b>Score</b> then by <b>Games Completed</b></p>
+    <p class="sortedExplanation">Sorted by: <b>1) Score</b> then by <b>2) Games Completed</b> then by <b>3) Most Gold Medals 4) Most Silver Medals 5) Most Bronze Medals</b></p>
     {/if}
 </div>
 
